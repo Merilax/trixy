6f0e8e83fdd94b470bee326b302c8875805a316c
Glitch (trixyv2) <none>
2021-09-18 20:22:20 +0200

    generic

---
+++ commands/DB/db.js
@@ -0,0 +1,111 @@
+const Sequelize = require('sequelize');
+//SQLite3
+/* 
+const sequelize = new Sequelize('database', 'user', 'password', {
+    host: 'localhost',
+    dialect: 'sqlite',
+    logging: false,
+    storage: 'database.sqlite',
+});
+ */
+
+// PostGreSQL
+
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialect: 'postgres',
+    protocol: 'postgres',
+    dialectOptions: {
+        ssl: {
+            require: true,
+            rejectUnauthorized: false
+        }
+    }
+});
+
+
+sequelize.authenticate();
+console.log("Database Auth Success!");
+
+const XPEnabled = sequelize.define('xpenable', {
+    guild: {
+        type: Sequelize.STRING,
+        allowNull: false,
+    },
+    enabled: {
+        type: Sequelize.BOOLEAN,
+        defaultValue: false,
+    },
+});
+
+const Levels = sequelize.define('levels', {
+    user: {
+        type: Sequelize.STRING,
+        allowNull: false,
+    },
+    userId: {
+        type: Sequelize.STRING,
+        allowNull: false,
+    },
+    guild: {
+        type: Sequelize.STRING,
+        allowNull: false,
+    },
+    level: {
+        type: Sequelize.INTEGER,
+        defaultValue: 0,
+        allowNull: false,
+    },
+    xp: {
+        type: Sequelize.INTEGER,
+        defaultValue: 15,
+        allowNull: false,
+    },
+    message_count: {
+        type: Sequelize.INTEGER,
+        defaultValue: 1,
+        allowNull: false,
+    },
+});
+
+const Mutes = sequelize.define('mutes', {
+    userName: {
+        type: Sequelize.STRING,
+        allowNull: false,
+    },
+    userId: {
+        type: Sequelize.STRING,
+        allowNull: false,
+    },
+    guildId: {
+        type: Sequelize.STRING,
+        allowNull: false,
+    },
+    duration: Sequelize.INTEGER,
+});
+
+const Reminders = sequelize.define('reminders', {
+    userId: {
+        type: Sequelize.STRING,
+        allowNull: false,
+    },
+    text: {
+        type: Sequelize.STRING(1999),
+        allowNull: false,
+    },
+    duration: Sequelize.INTEGER,
+});
+
+const Prefix = sequelize.define('prefix', {
+    guildId: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true,
+    },
+    prefix: {
+        type: Sequelize.STRING(10),
+        allowNull: false,
+    },
+});
+
+sequelize.sync({force:false});
+module.exports = { XPEnabled, Levels, Mutes, Reminders, Prefix };

a16db1128e06b26233c1da4f08ec318202aefc9e
Glitch (trixyv2) <none>
2021-09-24 17:25:49 +0200

    etc

--- commands/system/help.js
+++ commands/system/help.js
@@ -42,7 +42,7 @@ module.exports.run = (
         "<:settings:614100269004750898> **I'd love to help! Listing all commands:**"
       )
       .setDescription(
-        "Need help? [Join the support server!](https://discord.gg/ZbUQ8Mh)\nPut a command after `Trixy, help` for more info about it.\nHave you seen our new website? Check it out [here](https://merilax.github.io/trixy-front)!"
+        "Need help? [Join the support server!](https://discord.gg/ZbUQ8Mh)\nPut a command after `Trixy, help` for more info about it.\nHave you seen our new website? Check it out [here](https://trixy-v2.herokuapp.com)!"
       )
       .addField(
         "**System and information:**",
